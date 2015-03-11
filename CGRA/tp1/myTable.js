fd523b8d0ab176c520f95df6a50cc341f4da70e5
jrfcabral <joaofigueiredocabral@gmail.com>
2015-03-11 15:47:50 +0000

    Arranjado problema com translações da mesa
    Adicionado um pushMatrix() e um popMatrix() para reverter a translação
    associada à mesa

--- CGRA/tp1/myTable.js
+++ CGRA/tp1/myTable.js
@@ -9,7 +9,10 @@ myTable.prototype = Object.create(CGFobject.prototype);
 myTable.prototype.constructor=myTable;
 
 myTable.prototype.display = function(){
+	
+	this.scene.pushMatrix();
 	this.scene.translate(0, 1.8, 0);
+	
 	this.scene.pushMatrix();
 	this.scene.translate((-2.5)+0.15, 0, (-1.5)+0.15);
 	this.scene.scale(0.3, 3.5, 0.3);
@@ -38,6 +41,8 @@ myTable.prototype.display = function(){
 	this.scene.translate(0, 1.9, 0);
 	this.scene.scale(5.0, 0.3, 3.0);
 	this.cube.display();
+	this.scene.popMatrix();
+	
 	this.scene.popMatrix();
 
 };
