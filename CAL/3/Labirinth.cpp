3da851f3d0d3c282142d6b741dd2f148d3bfbb75
Joao Rafael de Figueiredo Cabral <up201304395@fe.up.pt>
2015-03-12 15:46:51 +0000

    Ficheiros fornecidos para a aula nº3

---
+++ CAL/3/Labirinth.cpp
@@ -0,0 +1,48 @@
+/*
+ * labirinth.cpp
+ */
+
+#include "Labirinth.h"
+
+#include <iostream>
+using namespace std;
+
+
+
+Labirinth::Labirinth(int values[10][10])
+{
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			labirinth[i][j]=values[i][j];
+}
+
+
+void Labirinth::initializeVisited(int x, int y)
+{
+	for (int i = 0; i < 10; i++)
+		for (int a = 0; a < 10; a++)
+			visited[i][a] = false;
+	visited[y][x] = true;
+}
+
+
+
+
+void  Labirinth::printLabirinth()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int a = 0; a < 10; a++)
+			cout << labirinth[i][a] << " ";
+
+		cout << endl;
+	}
+}
+
+
+bool Labirinth::findGoal(int x, int y)
+{
+	return false;
+}
+
+
