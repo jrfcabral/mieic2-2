5a41271381ad05a20a013d77858a1f6fb4f6485c
joaofigueiredocabral@gmail.com <joaofigueiredocabral@gmail.com>
2015-04-02 18:11:44 +0100

    Primeira versão com menu principal funcional

--- LPOO/ProjetoGuiado/src/labirinto/gui/Interface.java
+++ LPOO/ProjetoGuiado/src/labirinto/gui/Interface.java
@@ -1,23 +1,54 @@
 package labirinto.gui;
 
-import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
-import labirinto.logic.Labirinto;
-
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.CardLayout;
 import java.awt.BorderLayout;
 
-public class Interface {
-	
-	private Labirinto labirinto;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Interface {
+
+	private static final String MAIN_MENU = "Menu";
+	private static final String BUILD = "Build";
+	private static final String PLAY = "Play";
+	private static final String TUTORIAL = "Tutorial";
+	
 	private JFrame frame;
-	private JPanel panelLabirinto;
+	private JPanel menu;
+	private JPanel labirinto;
+	private JPanel editor;
+	
+	class MenuListener implements ActionListener {
+		private String newCard;
+		
+		public MenuListener(String newCard){
+			this.newCard = newCard;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {			
+			if (frame.getLayout() != null && frame.getLayout() instanceof CardLayout){
+				((CardLayout) frame.getLayout()).show(frame, newCard);;
+				
+			}
+		}
+		
+	};
 
 	/**
 	 * Launch the application.
@@ -37,41 +68,53 @@ public class Interface {
 
 	/**
 	 * Create the application.
-	 * @throws UnsupportedLookAndFeelException 
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
-	 * @throws ClassNotFoundException 
 	 */
-	public Interface() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		labirinto = new Labirinto();
+	public Interface() {
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
-	 * @throws ClassNotFoundException 
-	 * @throws InstantiationException 
-	 * @throws IllegalAccessException 
 	 */
-	private void initialize() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		
-	
-		
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1000, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.setLayout(null);
-		frame.setPreferredSize(new Dimension(1000,500));
-		
-		panelLabirinto = new PainelLabirinto(labirinto);		
-		panelLabirinto.setBounds(0, 0, 450, 300);
-		Dimension d = new Dimension(frame.getWidth(), frame.getHeight());
-		panelLabirinto.setPreferredSize(d);
-		frame.getContentPane().add(panelLabirinto);
-		frame.pack();
-		
-		
+	private void initialize() {
+		initApplicationFrame();		
+		initMenu();		
 	}
 
+	private void initApplicationFrame() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Uma aventura no labirinto encantado");
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+	}
+
+	private void initMenu() {
+		menu = new JPanel();
+		menu.setLayout(new BorderLayout());
+		JLabel mazelbl = new JLabel();
+		mazelbl.setText("Bem vindo à masmorra");
+		
+		menu.add(mazelbl, BorderLayout.CENTER);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		JButton buttonTutorial = new JButton();
+		JButton buttonPlay = new JButton();
+		JButton buttonBuild = new JButton();
+		
+		buttonTutorial.addActionListener(new MenuListener(TUTORIAL));
+		buttonPlay.addActionListener(new MenuListener(PLAY));
+		buttonBuild.addActionListener(new MenuListener(BUILD));
+		
+		buttonTutorial.setText(TUTORIAL);
+		buttonPlay.setText(PLAY);
+		buttonBuild.setText("Maze builder");
+		buttonPanel.add(buttonBuild);
+		buttonPanel.add(buttonTutorial);
+		buttonPanel.add(buttonPlay);
+		menu.add(buttonPanel, BorderLayout.SOUTH);
+		
+		frame.add(menu, MAIN_MENU);
+	}
 }
