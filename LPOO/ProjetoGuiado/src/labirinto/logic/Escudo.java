a6a7ee01bdf3483c1cf21043561bf76ab0fa9d80
Areadrill <jackthemotorcycle@gmail.com>
2015-04-10 19:46:32 +0100

    Documentation done for the Escudo class done.

--- LPOO/ProjetoGuiado/src/labirinto/logic/Escudo.java
+++ LPOO/ProjetoGuiado/src/labirinto/logic/Escudo.java
@@ -1,6 +1,14 @@
 package labirinto.logic;
 
+/**
+ * Class that represents the game's shield
+ *
+ */
 public class Escudo extends Peca{
+	/**
+	 * Constructor for the Escudo class.
+	 * @param posicao Position within the maze where the shield will be placed.
+	 */
 	public Escudo(Posicao posicao){
 		super(posicao);
 	}
