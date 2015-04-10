4daf1d12da2c1d9f99943275cb6c321985c8ad01
Areadrill <jackthemotorcycle@gmail.com>
2015-04-10 19:47:47 +0100

    Documentation for the Espada class done.

--- LPOO/ProjetoGuiado/src/labirinto/logic/Espada.java
+++ LPOO/ProjetoGuiado/src/labirinto/logic/Espada.java
@@ -1,6 +1,14 @@
 package labirinto.logic;
 
+/**
+ * Class that represents the game's sword.
+ *
+ */
 public class Espada extends Peca {
+	/**
+	 * Constructor for the Espada class.
+	 * @param posicao Position within the maze where the sword will be placed.
+	 */
 	public Espada(Posicao posicao)
 	{
 		super(posicao);
