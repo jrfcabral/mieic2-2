a469c59a00aee6a9e771b4c104145367a566cbfc
Areadrill <jackthemotorcycle@gmail.com>
2015-04-10 19:35:48 +0100

    Documentation for the Dardo class dne.

--- LPOO/ProjetoGuiado/src/labirinto/logic/Dardo.java
+++ LPOO/ProjetoGuiado/src/labirinto/logic/Dardo.java
@@ -2,7 +2,15 @@ package labirinto.logic;
 
 import java.io.Serializable;
 
+/**
+ * Class that represents the game's javelin.
+ */
 public class Dardo extends Peca implements Serializable{
+	
+	/**
+	 * Constructor for the Dardo class
+	 * @param posicao Position within the maze where the instance will be placed.
+	 */
 	public Dardo(Posicao posicao){
 		super(posicao);
 	}
