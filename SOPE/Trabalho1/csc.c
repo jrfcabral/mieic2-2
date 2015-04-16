32985538a318f0333ac326200771a17e5da8e65d
joaofigueiredocabral@gmail.com <joaofigueiredocabral@gmail.com>
2015-04-16 02:03:43 +0100

    fuciona com o cat, por alguma razao o gedit so ve carateres aleatorios

--- SOPE/Trabalho1/csc.c
+++ SOPE/Trabalho1/csc.c
@@ -2,36 +2,158 @@
 //João Cabral & João Mota
 //csc.c
 //created 15th of April 2015
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
 #include <string.h>
 
-int main()
+//calls cat with the arguments provided by args, outputs to fd
+void concatenate(char** args, int fd){
+    int backup = dup(STDOUT_FILENO);
+    dup2(fd, STDOUT_FILENO);    
+    int pid = fork();
+    if (pid < 0){
+        perror(strerror(errno));
+        exit(errno);
+    }
+    else if (pid == 0){
+        wait(&pid);
+        if (pid)
+            puts("cat returned an error code");
+    }
+    else if(execvp(*args, args) < 0){
+        perror(strerror(errno));
+        exit(errno);
+    }  
+        
+    dup2(backup, STDOUT_FILENO);   
+}
+
+//calls sort on the file provided by path, outputs to fd
+void sort(char* path){
+    int pid = fork();
+    if (pid < 0){
+        perror(strerror(errno));
+        exit(errno);
+    }
+    else if (pid == 0){
+        wait(&pid);
+        if (pid)
+            puts("sort returned an error code");
+    }
+    else if(execlp("sort", "sort", path, "-o", path, NULL) < 0){
+        perror(strerror(errno));
+        exit(errno);
+    }
+}
+
+//joins lines that have the same first word, inputs from inpath, outputs to outpath
+void clean(char* inpath, char* outpath){
+    if(!strcmp(inpath, outpath)){
+        perror("clean: cannot output to the file being read\n");
+        exit(-1);
+    }
+    
+    //redirect input and output
+    int backup_in = dup(STDIN_FILENO);
+    int backup_out = dup(STDOUT_FILENO);
+    int in = open(inpath, (O_RDONLY), 0777);
+    int out = open(outpath, (O_CREAT | O_TRUNC | O_WRONLY), 0777);
+    dup2(in, STDIN_FILENO);
+    dup2(out, STDOUT_FILENO);
+    if (backup_in < 0 || backup_out < 0 || in < 0 || out < 0){
+        perror("clean: couldn't redirect output!");
+        exit(-1);
+    }
+    
+    //call awk
+    int pid = fork();
+    if (pid < 0){
+        perror(strerror(errno));
+        exit(errno);
+    }
+    else if (pid == 0){
+       wait(&pid);
+       if (pid)
+           puts("clean returned an error code");
+    }
+    else if (execlp("awk", "awk", "{line=\"\";for(i=2;i <= NF; i++)line = line $i \" \"; table[$1] = table[$1] line;} END {for (key in table) print key \" \" table[key] \n;}", NULL) < 0){
+        perror(strerror(errno));
+        exit(errno);
+    }    
+    //restore output/input
+    dup2(backup_in, STDIN_FILENO);
+    dup2(backup_out, STDOUT_FILENO);
+    close(in);
+    close(out);
+}
+
+int main(int argc, char** argv)
 {
     //try to open current directory and fail on error
     char currentdir[1024];
-    getcwd(currentdir, 1024);
+    if (argc == 1)
+        getcwd(currentdir, 1024);
+    else if (argc == 2)
+        strcpy(currentdir, argv[0]);
+    else
+        printf("Usage: csc directory\n");
+    
 	DIR* dir = opendir(currentdir);
    	if (dir == NULL){
 	    perror(strerror(errno));
 	    exit(errno);
 	}  
 
-    //iterate over the directory entries to find the ones that begin with res_
+    //iterate over the directory entries to find the ones that begin with res_ and save them to a vector of strings
     struct dirent *entry;
     char prefix[] = "res_";
     char **found = malloc(sizeof(char*));
-    for(entry = readdir(dir);entry != NULL; entry = readdir(dir)){
-        if (strncmp(prefix, entry->d_name, 4) == 0)
-            printf("%s", entry->d_name);
-    }
-    //two things can cause readdir to return NULL - an error or no more files to read
+    found[0] = malloc(sizeof(char)*strlen("cat"));
+    strcpy(found[0], "cat");    
+    int foundSize = 1;
+    for(entry = readdir(dir);entry != NULL; entry = readdir(dir))
+        if (strncmp(prefix, entry->d_name, 4) == 0){
+            found = realloc(found, ++foundSize*sizeof(char*));  
+            found[foundSize-1] = malloc(sizeof(char)*strlen(entry->d_name));
+            strcpy(found[foundSize-1], entry->d_name);            
+        }
+    
+    //two things can cause readdir after iterate to return NULL - an error or no more files to read
     if (errno){
         perror(strerror(errno));
         exit(errno);
     }
+    
+    //open output file
+    int temp = open("temp.txt", (O_CREAT | O_TRUNC | O_WRONLY), 0777);   
+    
+    //concatenate the files
+    if (foundSize > 1){ //if there is anything to do       
+        found = realloc(found, ++foundSize*sizeof(char*));
+        found[foundSize-1] = NULL; //arg array needs to end in null for execvp
+        concatenate(found, temp);       
+    }
+    else{
+        close(temp);
+        exit(0);//no files to index, exit successfully
+    }
+    
+    //sort the concatenated file   
+    sort("temp.txt");
+
+    //join lines started by the same word
+    //clean("temp.txt", "index.txt");
+    // if(unlink("temp.txt")) perror("Couldn't delete temporary file");
+    
+    //release allocated memory
+    for(;foundSize>0;foundSize--)free(found[foundSize-1]);
+    free(found);
+    exit(0);//successful run
 }
