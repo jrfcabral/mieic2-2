568762db830165fb83e9296b6597534c9b6c2631
jrfcabral <joaofigueiredocabral@gmail.com>
2015-05-27 01:36:12 +0200

    mais um

--- BDAD/lid.sql
+++ BDAD/lid.sql
@@ -49,9 +49,20 @@ GROUP BY Cela.idLocal
 HAVING COUNT(Prisioneiro.idPessoa) > 1;
 
 --Mostra descriçao de todos os incidentes em que cada funcionário foi relator
-SELECT Pessoa.nome, Incidente.descricao FROM Pessoa INNER JOIN Funcionario ON Pessoa.idPessoa = Funcionario.idPessoa INNER JOIN Incidente ON Incidente.relator = Funcionario.idPessoa;
+SELECT Pessoa.nome, Incidente.descricao FROM Pessoa INNER JOIN Funcionario ON Pessoa.idPessoa = Funcionario.idPessoa INNER JOIN Incidente ON Incidente.relator = Funcionario.idPessoa
+ORDER BY Pessoa.nome;
 
 --Mostra todos os prisioneiros cuja primeira sentença foi pronunciada este mês
 SELECT Pessoa.nome, MIN(Pena.dataDeSentenca) as dataDePrimeiraSentenca, Pena.motivo FROM Pena INNER JOIN Pessoa ON Pena.idPessoa = Pessoa.idPessoa
 GROUP BY (Pessoa.idPessoa)
 HAVING MIN(Pena.dataDeSentenca) >= datetime('now', '-1 months');
+
+--Mostrar numero de objetos pessoais que os presos estao autorizados a ter em cada cela
+SELECT Cela.numero, COUNT(ObjetoPessoal.idRecompensa) AS NumeroDeObjetos FROM Cela INNER JOIN Prisioneiro ON Prisioneiro.cela = Cela.idLocal INNER JOIN Recompensa ON Recompensa.idPrisioneiro = Prisioneiro.idPessoa INNER JOIN ObjetoPessoal ON ObjetoPessoal.idRecompensa = Recompensa.idRecompensa
+GROUP BY (Cela.numero);
+
+--Mostrar celas em que pelo menos 2 prisioneiros estiveram envolvidos no mesmo incidente
+SELECT Cela.numero, COUNT(Pessoa.idPessoa) AS NumeroDePrisioneirosInvolvidosNoMesmoIncidente FROM Pessoa INNER JOIN Prisioneiro ON Pessoa.idPessoa = Prisioneiro.idPessoa INNER JOIN Cela ON Prisioneiro.cela = Cela.idLocal INNER JOIN PrisioneiroIncidente ON PrisioneiroIncidente.prisioneiro = Prisioneiro.idPessoa 
+INNER JOIN Incidente ON Incidente.idIncidente = PrisioneiroIncidente.incidente
+GROUP BY idIncidente, cela
+HAVING COUNT(Pessoa.idPessoa) > 1;
