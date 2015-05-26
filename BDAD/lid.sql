ab92527e95d1eebab2663adf06a51541be88663c
jrfcabral <joaofigueiredocabral@gmail.com>
2015-05-27 01:27:35 +0200

    mais queries

--- BDAD/lid.sql
+++ BDAD/lid.sql
@@ -1,3 +1,4 @@
+--impossibilita adicionar recompensas a prisioneiros castigados nos ultimos 30 dias
 CREATE TRIGGER periodoDeNojo
 BEFORE INSERT ON Recompensa
 FOR EACH ROW
@@ -9,12 +10,14 @@ THEN RAISE(ABORT, 'Prisioneiro não pode ser recompensado pois cometeu infraçã
 END;
 END;
 
+--remove recompensas quando é aplicada uma penalizaçao
 CREATE TRIGGER removeRecompensas
 AFTER INSERT ON Penalizacao
 BEGIN
 DELETE FROM Recompensa WHERE Recompensa.idPrisioneiro = new.idPrisioneiro;
 END;
 
+--remove objeto recompensa quando a recompensa correspondente for removida
 CREATE TRIGGER removeObjeto
 AFTER DELETE ON Recompensa
 FOR EACH ROW
@@ -22,6 +25,7 @@ BEGIN
 DELETE FROM ObjetoPessoal WHERE OLD.idRecompensa = ObjetoPessoal.idRecompensa;
 END;
 
+-- remove tempo de recreio quando recompensa correspondente for removida
 CREATE TRIGGER removeRecreio
 AFTER DELETE ON Recompensa
 FOR EACH ROW
@@ -29,13 +33,25 @@ BEGIN
 DELETE FROM TempoDeRecreio WHERE OLD.idRecompensa = TempoDeRecreio.idRecompensa;
 END;
 
+--mostra horas dos recreios
 SELECT nome, horaInicio, horaFim FROM Prisioneiro, Recompensa, TempoDeRecreio, Pessoa
 WHERE Pessoa.idPessoa = Prisioneiro.idPessoa AND Prisioneiro.idPessoa = Recompensa.idPrisioneiro AND TempoDeRecreio.idRecompensa = Recompensa.idRecompensa;
 
---Reincidentes
+--Mostra prisioneiros com mais que uma pena
 SELECT nome, COUNT(Pena.idPena) as Penas FROM Pena INNER JOIN Pessoa
 ON Pessoa.idPessoa = Pena.idPessoa
 GROUP BY Pessoa.idPessoa
 HAVING COUNT(Pena.idPena) > 1;
 
---
+--Mostra celas com mais do que um prisioneiro
+SELECT Local.nome, Local.funcao, COUNT(Prisioneiro.idPessoa) AS NumeroDePrisioneiros FROM Prisioneiro INNER JOIN Pessoa ON Prisioneiro.idPessoa = Pessoa.idPessoa INNER JOIN Cela ON Prisioneiro.cela = Cela.idLocal INNER JOIN Local ON Local.idLocal = Cela.idLocal
+GROUP BY Cela.idLocal
+HAVING COUNT(Prisioneiro.idPessoa) > 1;
+
+--Mostra descriçao de todos os incidentes em que cada funcionário foi relator
+SELECT Pessoa.nome, Incidente.descricao FROM Pessoa INNER JOIN Funcionario ON Pessoa.idPessoa = Funcionario.idPessoa INNER JOIN Incidente ON Incidente.relator = Funcionario.idPessoa;
+
+--Mostra todos os prisioneiros cuja primeira sentença foi pronunciada este mês
+SELECT Pessoa.nome, MIN(Pena.dataDeSentenca) as dataDePrimeiraSentenca, Pena.motivo FROM Pena INNER JOIN Pessoa ON Pena.idPessoa = Pessoa.idPessoa
+GROUP BY (Pessoa.idPessoa)
+HAVING MIN(Pena.dataDeSentenca) >= datetime('now', '-1 months');
